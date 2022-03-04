5c2e9f8d254554f466e64595fd7834a8b48bcb49
ManuHervas <hervasnavarrete90@gmail.com>
2022-03-04 09:10:05 +0100

    commit

--- src/javadocedd/JavaDocEDD.java
+++ src/javadocedd/JavaDocEDD.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Calculadora sencilla en java con capacidad
+ * para sumar, restar, multiplicar y dividir
  */
 package javadocedd;
 
@@ -9,65 +8,19 @@ import java.util.Scanner;
 
 /**
  *
- * @author Manuel
+ * @author Manuel Hervás
  */
+
 public class JavaDocEDD {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // declaración del metodo main
     
-        String name; // El valor inicial se puede omitir aquí, el nombre predeterminado es nulo
-
-	 System.out.println ("bienvenido, ven a la computadora perenne");
-
-	 String exit = ""; // = nulo; informará una excepción de puntero nulo
-	while(!exit.equals("exit")) {
-		// ctrl + alt + o
-		Scanner scanner = new Scanner(System.in);
-		 System.out.println ("Introduzca su primer número:");
-		 // El método scanner.nextDouble bloqueará el programa aquí
-		double firstNum = scanner.nextDouble();
-
-		 System.out.println ("Introduzca su segundo número:");
-		double secondNum = scanner.nextDouble();
-
-		 System.out.println ("Introduzca la operación que desea realizar: + - * /");
-		 String caoZuo = scanner.next (); // siguiente es string
-		
-		 // romper; Saltar de todo el bloque de código, el bloque de código ha terminado
-		switch (caoZuo) {
-
-			case "+":
-				 System.out.println ("El resultado es:" + (firstNum + secondNum));
-				break;
-			case "-":
-				 System.out.println ("El resultado es:" + (firstNum-secondNum));
-				break;
-			case "*":
-				 System.out.println ("El resultado es:" + (firstNum * secondNum));
-				break;
-			case "/":
-				 System.out.println ("El resultado es:" + (firstNum / secondNum));
-				break;
-			default:
-				 System.out.println ("¡Hombre, no hagas problemas! ¡Por favor, pórtate bien!");
-				 break; // Se puede omitir el break en default
-		}
-		
-		 System.out.println ("¿Desea continuar con el cálculo? Si desea salir, ingrese exit, si continúa, ingrese cualquier carácter!");
-		exit = scanner.next();
-		if(exit.equals("exit")) {
-			 System.out.println ("¡Bienvenido a usar la próxima vez!");
-		}
-		
-	
-	
+        doc javadoc = new doc();
 
 }
-
-}
-
+    
 }
--- src/javadocedd/doc.java
+++ src/javadocedd/doc.java
@@ -1,14 +1,71 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+*clase separada del metodo main y de la clase principal
  */
 package javadocedd;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Manuel
  */
 public class doc {
-    
+
+    public doc() {
+        String name; // El valor inicial se puede omitir aquí, el nombre predeterminado es nulo
+
+	 System.out.println ("bienvenido, ven a la computadora perenne");
+
+	 String exit = ""; // = nulo; informará una excepción de puntero nulo
+	while(!exit.equals("exit")) {
+		// ctrl + alt + o
+		Scanner scanner = new Scanner(System.in);
+		 System.out.println ("Introduzca su primer número:");
+		 // El método scanner.nextDouble bloqueará el programa aquí
+		double firstNum = scanner.nextDouble();
+
+		 System.out.println ("Introduzca su segundo número:");
+		double secondNum = scanner.nextDouble();
+
+		 System.out.println ("Introduzca la operación que desea realizar: + - * /");
+		 String caoZuo = scanner.next (); // siguiente es string
+		
+		 // romper; Saltar de todo el bloque de código, el bloque de código ha terminado
+		switch (caoZuo) {
+
+			case "+":
+				 System.out.println ("El resultado es:" + (firstNum + secondNum));
+				break;
+			case "-":
+				 System.out.println ("El resultado es:" + (firstNum-secondNum));
+				break;
+                                
+                                //hasta aquí tenemos los 2 metodos para realizar las sumas
+                                
+			case "*":
+				 System.out.println ("El resultado es:" + (firstNum * secondNum));
+				break;
+			case "/":
+				 System.out.println ("El resultado es:" + (firstNum / secondNum));
+				break;
+			default:
+				 System.out.println ("¡Hombre, no hagas problemas! ¡Por favor, pórtate bien!");
+				 break; // Se puede omitir el break en default
+		}
+		
+		 System.out.println ("¿Desea continuar con el cálculo? Si desea salir, ingrese exit, si continúa, ingrese cualquier carácter!");
+		exit = scanner.next();
+		if(exit.equals("exit")) {
+			 System.out.println ("¡Bienvenido a usar la próxima vez!");
+		}
+		
+	
+	
+
 }
+
+}
+    }
+    
+    
+
